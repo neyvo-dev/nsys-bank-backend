859d2ef383076571c1489f2e42baf07cd57c13af
neyvo.souza <neyvo.souza@instivo.com.br>
2024-09-13 13:03:07 -0500

    feat: listando notificações com paginação

---
+++ src/main/java/com/neyvo/nsysbank/notificacao/NotificacaoController.java
@@ -0,0 +1,24 @@
+package com.neyvo.nsysbank.notificacao;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notificacao")
+@AllArgsConstructor
+public class NotificacaoController {
+
+  private final NotificacaoRepository notificacaoRepository;
+
+  @GetMapping
+  public Page<Notificacao> listar(Pageable pageable) {
+
+    return notificacaoRepository.findAll(pageable);
+
+  }
+
+}
--- src/main/java/com/neyvo/nsysbank/notificacao/NotificacaoRepository.java
+++ src/main/java/com/neyvo/nsysbank/notificacao/NotificacaoRepository.java
@@ -2,9 +2,11 @@ package com.neyvo.nsysbank.notificacao;
 
 import java.util.UUID;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificacaoRepository extends ListCrudRepository<Notificacao, UUID> {
+public interface NotificacaoRepository extends ListCrudRepository<Notificacao, UUID>,
+    PagingAndSortingRepository<Notificacao, UUID> {
 
 }
